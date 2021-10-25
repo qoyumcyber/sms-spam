34e34af17662f19441424e5f5f3b57aa2ded4c0f
qoyumcyber <qoyumcyber@gmail.com>
2021-10-25 10:40:32 +0700

    Initial commit

---
+++ intro.php
@@ -0,0 +1,22 @@
+<?php
+
+$banner = "\e[36;1m                                                                                 
+                                                                                 
+    ######   ######  #     #
+   #      # #      # #     #     
+   #      # #      # #     # 
+   #      # #      # #     # 
+   #   #  # #      # #     #          
+    ######   ######   #####         
+        #               #
+         #              #      
+                                                                                 
+[#] Spam SMS [#]    
+                                   
+Author : Qoy Cyber                  
+Team   : Lulzsec                   
+Github : https//github.com/qoyumcyber/\n\e[0;1m";
+                                                                                 
+                                                                                                                                                                 
+sleep(3);
+echo $banner;
